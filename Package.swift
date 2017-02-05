4c1e3a62618a47b320d6462296d738a7f940f1dc
Sven Straubinger <straubinger.es@googlemail.com>
2017-02-05 15:02:32 +0100

    Add dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "kitura-server-swift"
+    name: "kitura-server-swift",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4)
+    ]
 )
