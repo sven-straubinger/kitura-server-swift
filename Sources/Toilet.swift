62de2ba45368fdde4b41db30ad90c60d7abff053
Sven Straubinger <straubinger.es@googlemail.com>
2017-02-06 23:08:11 +0100

    Add constructor

--- Sources/Toilet.swift
+++ Sources/Toilet.swift
@@ -14,6 +14,12 @@ class Toilet: NSObject {
     var locked = false
     var name   = "RT-1"
     
+    init(_ id: Int,_ locked: Bool,_ name: String) {
+        self.id = id
+        self.locked = locked
+        self.name = name
+    }
+    
     func toJSON() -> Dictionary<String, Any> {
         return [
             "id": self.id,
@@ -24,10 +30,10 @@ class Toilet: NSObject {
     
     static func dummies() -> [Toilet] {
         var allVendors = Array<Toilet>()
-        allVendors.append(Toilet());
-        allVendors.append(Toilet());
-        allVendors.append(Toilet());
-        allVendors.append(Toilet());
+        allVendors.append(Toilet(1,true,"RT-1"));
+        allVendors.append(Toilet(2,false,"S-1"));
+        allVendors.append(Toilet(3,true,"RT-2"));
+        allVendors.append(Toilet(4,false,"S-2"));
         
         return allVendors
     }
